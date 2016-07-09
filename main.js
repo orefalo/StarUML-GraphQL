56956d17c825d63bb858b4c14e4157c9a312bf1f
Thomas Min <thomas.min.git@gmail.com>
2016-07-09 18:34:33 +0900

    Add dummy code gen?

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ define(function(require, exports, module) {
     var Commands = app.getModule('command/Commands'),
         CommandManager = app.getModule("command/CommandManager"),
         MenuManager = app.getModule("menu/MenuManager"),
-        //  ElementPickerDialog = app.getModule("dialogs/ElementPickerDialog"),
         FileSystem = app.getModule("filesystem/FileSystem"),
+        ElementPickerDialog = app.getModule("dialogs/ElementPickerDialog"),
         Dialogs = app.getModule("dialogs/Dialogs"),
 
-        // JSGen = require("JSCodeGenerator"),
+        TypeScriptCodeGenerator = require("TypeScriptCodeGenerator"),
         TypeScriptConfigure = require("TypeScriptConfigure");
 
     var OUTER_CMD = "tools.typescript",
@@ -21,41 +21,73 @@ define(function(require, exports, module) {
         CMD_ABOUT = "tools.typescript.about",
         CMD_CONFIG = "tools.typescript.configure";
 
-    function generateTypeScript(base, path, opts) {
+    function generateTypeScript(base, path, options) {
         var result = new $.Deferred();
         console.log('base', base);
         console.log('path', path);
-        console.log('opts', opts);
+        console.log('options', options);
         // TODO generate ts code here
-        return result.resolve();
+        return TypeScriptCodeGenerator.generate(base, path, options).then(result.resolve, result.reject);
+        // return result.resolve();
     }
 
-    function handleGenerate(base, path, opts) {
+    function handleGenerate(base, path, options) {
         var result = new $.Deferred();
-        opts = TypeScriptConfigure.getGenOptions();
+        options = TypeScriptConfigure.getGenOptions();
         console.log('base', base);
         console.log('path', path);
-        console.log('opts', opts);
+        console.log('options', options);
         // window.alert("generated type script");
 
-        if (!path) {
-            FileSystem.showOpenDialog(false, true, "Select a folder where generated codes to be located", null, null,
-                function(err, files) {
-                    if (!err) {
-                        if (files.length > 0) {
-                            path = files[0];
-                            console.log('selected path', path);
-                            generateTypeScript(base, path, opts).then(result.resolve, result.reject);
+        // If base is not assigned, popup ElementPicker
+        if (!base) {
+            ElementPickerDialog.showDialog("Select a base model to generate codes", null, type.UMLPackage)
+                .done(function(buttonId, selected) {
+                    if (buttonId === Dialogs.DIALOG_BTN_OK && selected) {
+                        base = selected;
+
+                        // If path is not assigned, popup Open Dialog to select a folder
+                        if (!path) {
+                            FileSystem.showOpenDialog(false, true, "Select a folder where generated codes to be located", null, null, function(err, files) {
+                                if (!err) {
+                                    if (files.length > 0) {
+                                        path = files[0];
+                                        generateTypeScript(base, path, options).then(result.resolve, result.reject);
+                                    } else {
+                                        result.reject(FileSystem.USER_CANCELED);
+                                    }
+                                } else {
+                                    result.reject(err);
+                                }
+                            });
                         } else {
-                            result.reject(FileSystem.USER_CANCELED);
+                            generateTypeScript(base, path, options).then(result.resolve, result.reject);
                         }
                     } else {
-                        result.reject(err);
+                        result.reject();
                     }
-                }
-            );
+                });
         } else {
-            generateTypeScript(base, path, opts).then(result.resolve, result.reject);
+            // If path is not assigned, popup Open Dialog to select a folder
+            if (!path) {
+                FileSystem.showOpenDialog(false, true, "Select a folder where generated codes to be located", null, null,
+                    function(err, files) {
+                        if (!err) {
+                            if (files.length > 0) {
+                                path = files[0];
+                                console.log('selected path', path);
+                                generateTypeScript(base, path, options).then(result.resolve, result.reject);
+                            } else {
+                                result.reject(FileSystem.USER_CANCELED);
+                            }
+                        } else {
+                            result.reject(err);
+                        }
+                    }
+                );
+            } else {
+                generateTypeScript(base, path, options).then(result.resolve, result.reject);
+            }
         }
     }
     /// handleGenerate
